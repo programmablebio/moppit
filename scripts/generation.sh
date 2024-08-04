65c17ea7119a563c323b1ddedd7f89f1a2f363e4
TongChen <89793506+CTDancer@users.noreply.github.com>
2024-08-03 21:24:23 -0400

    Create generation.sh

---
+++ scripts/generation.sh
@@ -0,0 +1,15 @@
+export CUDA_VISBILE_DEVICES=7
+
+python moppit.py \
+-sm '/home/tc415/moPPIt/moppit/model_path/finetune_bindevaluator_0/model-epoch=29-val_mcc=0.60-val_loss=0.51.ckpt' \
+-n_layers 8 \
+-d_model 128 \
+-d_hidden 128 \
+-n_head 8 \
+-d_inner 64 \
+--protein_seq 'IVEGSDAEIGMSPWQVMLFRKSPQELLCGASLISDRWVLTAAHCLLYPPWDKNFTENDLLVRIGKHSRTRYERNIEKISMLEKIYIHPRYNWRENLDRDIALMKLKKPVAFSDYIHPVCLPDRETAASLLQAGYKGRVTGWGNLKETGQPSVLQVVNLPIVERPVCKDSTRIRITDNMFCAGYKPDEGKRGDACEGDSGGPFVMKSPFNNRWYQMGIVSWGEGCDRDGKYGFYTHVFRLKKWIQKVIDQFGE' \
+--peptide_length 11 \
+--motif '[18,23,59,67,68,69,70,76,77]' \
+--top_k 3 \
+--num_binders 50 \
+--num_display 10
