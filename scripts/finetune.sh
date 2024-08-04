12b9f1acdd52af19e3122611f848b6590bd2a719
TongChen <89793506+CTDancer@users.noreply.github.com>
2024-08-03 21:24:02 -0400

    Create finetune.sh

---
+++ scripts/finetune.sh
@@ -0,0 +1,15 @@
+export CUDA_VISIBLE_DEVICES=2,3,4,5,6,7
+
+torchrun --nproc_per_node=6 --master_port=25142 finetune_bindevaluator.py -o finetune_bindevaluator_0 \
+-sm '/home/tc415/moPPIt/moppit/model_path/train_bindevaluator_0/model-epoch=29-val_mcc=0.61.ckpt' \
+-lr 1e-4 \
+-n_layers 8 \
+-d_model 128 \
+-d_hidden 128 \
+-n_head 8 \
+-d_inner 64 \
+-batch_size 32 \
+--max_epochs 50 \
+-dropout 0.5 \
+--grad_clip 1 \
+--kl_weight 1
